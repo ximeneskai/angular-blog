aee52a595916392d01da68e1f604a8b250a931f3
Kai Ximenes <kaiximenes@outlook.com>
2024-11-05 12:34:51 -0300

    Componentes angulares

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuTitleComponent } from "./components/menu-title/menu-title.component";
+import { BigCardComponent } from "./components/big-card/big-card.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuTitleComponent, BigCardComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
